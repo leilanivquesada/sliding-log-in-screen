1fb4caa370b9f6f81af3164756560f5f5bcb4a82
Leilani Quesada <leilanivquesada@gmail.com>
2022-05-15 20:37:50 -0700

    completed exercise. for example, view https://codepen.io/leilanivquesada/pen/JjpbBQm

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const conainter = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => 
+signUpButton.addEventListener('click', ()=> 
     container.classList.add('right-panel-active')
 );
 
-signInButton.addEventListener('click', () => 
+signInButton.addEventListener('click', ()=> 
     container.classList.remove('right-panel-active')
 );
